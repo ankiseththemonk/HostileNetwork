8f964808adc113efee343dc9867b91b93ec0d7ba
Stephen Willson <stephendwillson@live.com>
2013-10-20 14:32:03 -0400

    Initial commit of source/sln.

---
+++ HostileNetworkClient/HostileNetworkClient.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[]) {
+
+	cout << "Hooray, client!" << endl;
+
+	return 0;
+}
+
---
+++ HostileNetworkServer/HostileNetworkServer.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[]) {
+
+	cout << "Hooray, server!" << endl;
+
+	return 0;
+}
+
